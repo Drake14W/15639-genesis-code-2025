af235badff675172918d9fb7388a921d2843122d
Drake Wiggins <drake_wiggins@outlook.com>
2025-09-20 01:02:52 -0700

    Added abstraction layer for gamepads, more framework (note the action map) to the teleop, and basic manual movement code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomGamepad.java
@@ -0,0 +1,286 @@
+package org.firstinspires.ftc.teamcode;
+
+import java.lang.Math;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class CustomGamepad {
+    Gamepad gamepad;
+    private boolean a, b, x, y;
+    private boolean a_just_pressed, b_just_pressed, x_just_pressed, y_just_pressed;
+
+    private double left_stick_x, left_stick_y, right_stick_x, right_stick_y;
+
+    private double left_trigger, right_trigger;
+
+    private boolean left_bumper, right_bumper;
+    private boolean left_bumper_just_pressed, right_bumper_just_pressed;
+
+    private boolean dpad_up, dpad_down, dpad_left, dpad_right;
+    private boolean dpad_up_just_pressed, dpad_down_just_pressed, dpad_left_just_pressed, dpad_right_just_pressed;
+
+    public CustomGamepad(Gamepad gamepad) {
+        this.gamepad = gamepad;
+
+        //Initialize all our values to 0
+
+        a = b = x = y = false;
+        a_just_pressed = b_just_pressed = x_just_pressed = y_just_pressed = false;
+
+        left_stick_x = left_stick_y = right_stick_x = right_stick_y = 0.0;
+
+        left_trigger = right_trigger = 0.0;
+
+        left_bumper = right_bumper = false;
+        left_bumper_just_pressed = right_bumper_just_pressed = false;
+
+        dpad_up = dpad_down = dpad_left = dpad_right = false;
+        dpad_up_just_pressed = dpad_down_just_pressed = dpad_left_just_pressed = dpad_right_just_pressed = false;
+    }
+
+    //Update controller inputs
+    public void update() {
+        //Lettered updates
+        if (gamepad.a) {
+            if (!a) {
+                a_just_pressed = true;
+            }
+            a = true;
+        }
+        if (gamepad.b) {
+            if (!b) {
+                b_just_pressed = true;
+            }
+            b = true;
+        }
+        if (gamepad.y) {
+            if (!y) {
+                y_just_pressed = true;
+            }
+            y = true;
+        }
+        if (gamepad.x) {
+            if (!x) {
+                x_just_pressed = true;
+            }
+            x = true;
+        }
+
+        //Stick updates
+        left_stick_x = gamepad.left_stick_x;
+        left_stick_y = -gamepad.left_stick_y;       //Note the negatives here. I'm flipping it because ftc is stupid and thinks up on the joystick is negative
+        right_stick_x = gamepad.right_stick_x;
+        right_stick_y = -gamepad.right_stick_y;     //Note the negatives here. I'm flipping it because ftc is stupid and thinks up on the joystick is negative
+
+        //Trigger updates
+        left_trigger = gamepad.left_trigger;
+        right_trigger = gamepad.right_trigger;
+
+        //Bumper updates
+        if (gamepad.left_bumper) {
+            if (!left_bumper) {
+                left_bumper_just_pressed = true;
+            }
+            left_bumper = true;
+        }
+        if (gamepad.right_bumper) {
+            if (!right_bumper) {
+                right_bumper_just_pressed = true;
+            }
+            right_bumper = true;
+        }
+
+        //D-pad updates
+        if (gamepad.dpad_up) {
+            if (!dpad_up) {
+                dpad_up_just_pressed = true;
+            }
+            dpad_up = true;
+        }
+        if (gamepad.dpad_down) {
+            if (!dpad_down) {
+                dpad_down_just_pressed = true;
+            }
+            dpad_down = true;
+        }
+        if (gamepad.dpad_left) {
+            if (!dpad_left) {
+                dpad_left_just_pressed = true;
+            }
+            dpad_left = true;
+        }
+        if (gamepad.dpad_right) {
+            if (!dpad_right) {
+                dpad_right_just_pressed = true;
+            }
+            dpad_right = true;
+        }
+    }
+
+    //Lettered buttons
+    public boolean get_a() {
+        return a;
+    }
+    public boolean get_b() {
+        return b;
+    }
+    public boolean get_x() {
+        return x;
+    }
+    public boolean get_y() {
+        return y;
+    }
+
+    //Lettered button raising edge inputs
+    public boolean get_a_just_pressed() {
+        return a_just_pressed;
+    }
+    public boolean get_b_just_pressed() {
+        return b_just_pressed;
+    }
+    public boolean get_x_just_pressed() {
+        return x_just_pressed;
+    }
+    public boolean get_y_just_pressed() {
+        return y_just_pressed;
+    }
+
+    //Stick inputs
+    //Angular deadzone in degrees, represents total angle in which it gets locked (not half)
+    public double get_left_stick_x(double angular_deadzone, double radial_deadzone) {
+        //Check radial deadzone
+        double temp_left_stick_x;
+        double temp_left_stick_y;
+        temp_left_stick_x = (left_stick_x > radial_deadzone) ? left_stick_x : 0.0;
+        temp_left_stick_y = (left_stick_y > radial_deadzone) ? left_stick_y : 0.0;
+
+        //Check angular deadzone
+        double angular_deadzone_radians = Math.toRadians(angular_deadzone);
+        double angle = Math.atan2(temp_left_stick_y, temp_left_stick_x);
+        if ((angle < ((Math.PI/2) + angular_deadzone_radians/2)) && (angle > ((Math.PI/2) - angular_deadzone_radians/2))) {
+            //In top angular deadzone
+            temp_left_stick_x = 0;
+        }
+        else if ((angle < ((0 - Math.PI/2) + angular_deadzone_radians/2)) && (angle > ((0 - Math.PI/2) - angular_deadzone_radians/2))) {
+            //In bottom angular deadzone
+            temp_left_stick_x = 0;
+        }
+
+        return temp_left_stick_x;
+    }
+    public double get_left_stick_y(double angular_deadzone, double radial_deadzone) {
+        //Check radial deadzone
+        double temp_left_stick_x;
+        double temp_left_stick_y;
+        temp_left_stick_x = (left_stick_x > radial_deadzone) ? left_stick_x : 0.0;
+        temp_left_stick_y = (left_stick_y > radial_deadzone) ? left_stick_y : 0.0;
+
+        //Check angular deadzone
+        double angular_deadzone_radians = Math.toRadians(angular_deadzone);
+        double angle = Math.atan2(temp_left_stick_y, temp_left_stick_x);
+        if ((angle < (0 + angular_deadzone_radians/2)) && (angle > (0 - angular_deadzone_radians/2))) {
+            //In right angular deadzone
+            temp_left_stick_y = 0;
+        }
+        else if ((angle < ((Math.PI) + angular_deadzone_radians/2)) && (angle > ((0 - Math.PI) - angular_deadzone_radians/2))) {
+            //In left angular deadzone
+            temp_left_stick_y = 0;
+        }
+
+        return temp_left_stick_y;
+    }
+    public double get_right_stick_x(double angular_deadzone, double radial_deadzone) {
+        //Check radial deadzone
+        double temp_right_stick_x;
+        double temp_right_stick_y;
+        temp_right_stick_x = (right_stick_x > radial_deadzone) ? right_stick_x : 0.0;
+        temp_right_stick_y = (right_stick_y > radial_deadzone) ? right_stick_y : 0.0;
+
+        //Check angular deadzone
+        double angular_deadzone_radians = Math.toRadians(angular_deadzone);
+        double angle = Math.atan2(temp_right_stick_y, temp_right_stick_x);
+        if ((angle < ((Math.PI/2) + angular_deadzone_radians/2)) && (angle > ((Math.PI/2) - angular_deadzone_radians/2))) {
+            //In top angular deadzone
+            temp_right_stick_x = 0;
+        }
+        else if ((angle < ((0 - Math.PI/2) + angular_deadzone_radians/2)) && (angle > ((0 - Math.PI/2) - angular_deadzone_radians/2))) {
+            //In bottom angular deadzone
+            temp_right_stick_x = 0;
+        }
+
+        return temp_right_stick_x;
+    }
+    public double get_right_stick_y(double angular_deadzone, double radial_deadzone) {
+        //Check radial deadzone
+        double temp_right_stick_x;
+        double temp_right_stick_y;
+        temp_right_stick_x = (right_stick_x > radial_deadzone) ? right_stick_x : 0.0;
+        temp_right_stick_y = (right_stick_y > radial_deadzone) ? right_stick_y : 0.0;
+
+        //Check angular deadzone
+        double angular_deadzone_radians = Math.toRadians(angular_deadzone);
+        double angle = Math.atan2(temp_right_stick_y, temp_right_stick_x);
+        if ((angle < (0 + angular_deadzone_radians/2)) && (angle > (0 - angular_deadzone_radians/2))) {
+            //In right angular deadzone
+            temp_right_stick_y = 0;
+        }
+        else if ((angle < ((Math.PI) + angular_deadzone_radians/2)) && (angle > ((0 - Math.PI) - angular_deadzone_radians/2))) {
+            //In right angular deadzone
+            temp_right_stick_y = 0;
+        }
+
+        return temp_right_stick_y;
+    }
+
+    //Trigger inputs
+    public double get_left_trigger() {
+        return left_trigger;
+    }
+    public double get_right_trigger() {
+        return right_trigger;
+    }
+
+    //Bumper inputs
+    public boolean get_left_bumper(){
+        return left_bumper;
+    }
+    public boolean get_right_bumper() {
+        return right_bumper;
+    }
+
+    //Bumper raising edge inputs
+    public boolean get_right_bumper_just_pressed() {
+        return right_bumper_just_pressed;
+    }
+    public boolean get_left_bumper_just_pressed() {
+        return left_bumper_just_pressed;
+    }
+
+    //D-Pad inputs
+    public boolean get_dpad_up() {
+        return dpad_up;
+    }
+    public boolean get_dpad_down() {
+        return dpad_down;
+    }
+    public boolean get_dpad_left() {
+        return dpad_left;
+    }
+    public boolean get_dpad_right() {
+        return dpad_right;
+    }
+
+    //D-Pad raising edge inputs
+    public boolean get_dpad_up_just_pressed() {
+        return dpad_up_just_pressed;
+    }
+    public boolean get_dpad_down_just_pressed() {
+        return dpad_down_just_pressed;
+    }
+    public boolean get_dpad_left_just_pressed() {
+        return dpad_left_just_pressed;
+    }
+    public boolean get_dpad_right_just_pressed() {
+        return dpad_right_just_pressed;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2024 ChessMan14, angeldescended
+Copyright (c) 2025 ChessMan14, angeldescended
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -32,6 +32,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.HashMap;
+import java.lang.Math;
 
 //This decorator puts this opmode into selected the name and group on the driver hub menu
 @TeleOp(name="MainTeleOp", group="Linear OpMode")
@@ -43,9 +44,7 @@ public class MainTeleOp extends LinearOpMode {
     //Map of all motors
     private HashMap<String, DcMotor> motors = new HashMap<>();
     //Map of wheel motor powers]
-    private HashMap<String, Double> wheel_motor_powers = new HashMap<>();
-    //Map of arm motor powers
-    private HashMap<String, Double> other_motor_powers = new HashMap<>();
+    private HashMap<String, Double> motor_powers = new HashMap<>();
 
     //Map of all servos
     private HashMap<String, Servo> servos = new HashMap<>();
@@ -57,25 +56,74 @@ public class MainTeleOp extends LinearOpMode {
     //Map of all CRServo powers
     private HashMap<String, Double> crservo_powers = new HashMap<>();
 
+    //Action map
+    //The structure for the action map works like this:
+    //Each entry represents an action like moving the robot manually or a macro
+    //Each bit in byte portion of the map entry is used to represent a part of the robot or some other "level" that the action needs to itself
+    //E.g    0 0 0 1 0 0 0
+    //             ^
+    //             |
+    //This bit might represent the arm and this byte is saying this macro needs the intake wheel.
+    //Since it is set to 1, this action needs the intake wheel all to itself.
+    //The last bit represents if the action is active.
+
+    //The levels:
+    //Level 0 (lsb):    General wheel control
+    //Level 1:          Rotation control
+    //Level 2:          Lorem ipsum
+    //Level 3:          Lorem ipsum
+    //Level 4:          Lorem ipsum
+    //Level 5:          Lorem ipsum
+    //Level 6:          Lorem ipsum
+    //Level 7:          On/Off
+    private HashMap<String, Byte> action_map = new HashMap<>();
+
+    //Custom gamepads (see CustomGamepad.java)
+    private CustomGamepad custom_gamepad_1, custom_gamepad_2;
+
+    //Used to describe movement
+    private double axial, lateral, trigger_yaw, stick_yaw, yaw;
+
+    //Globals for movement
+    double LONG_LAT_RADIAL_DEADZONE = 0.0;  //Decimal from 0-1
+    double LONG_LAT_ANGULAR_DEADZONE = 5;   //In degrees
+    double ROTATION_RADIAL_DEADZONE = 0.0;  //Decimal from 0-1
+    double ROTATION_ANGULAR_DEADZONE = 0.0;  //In degrees
+    double ROTATION_SPEED = 0.75;
+    double MOVEMENT_SPEED = 0.5;
+
+    //Used to set bytes to "on"
+    byte ON_BITMASK = (byte) 0b10000000;
+
+    private boolean check_mask(String key) {
+        for (String iter_key : action_map.keySet()) {
+            if (iter_key != key) {
+                if ((action_map.get(iter_key) & action_map.get(iter_key)) > 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     //We have to override this function since it has already been defined in the parent class LinearOpMode
     @Override
     public void runOpMode() {
+        //Add actions to hashmap
+        action_map.put("manual_movement", (byte) 0b00000001);
+        action_map.put("trigger_rotation", (byte) 0b00000010);
+        action_map.put("stick_rotation", (byte) 0b00000010);
+
         //Create and assign map entries for all motors
         motors.put("front_left", hardwareMap.get(DcMotor.class, "front_left_motor"));
         motors.put("back_left", hardwareMap.get(DcMotor.class, "back_left_motor"));
         motors.put("front_right", hardwareMap.get(DcMotor.class, "front_right_motor"));
         motors.put("back_right", hardwareMap.get(DcMotor.class, "back_right_motor"));
 
-        motors.put("slide", hardwareMap.get(DcMotor.class, "slide_motor"));
-        motors.put("arm", hardwareMap.get(DcMotor.class, "arm_motor"));
-        motors.put("actuator", hardwareMap.get(DcMotor.class, "actuator_motor"));
-
         //Create and assign map entries for all servos
-        servos.put("slide_servo", hardwareMap.get(Servo.class, "slide_servo"));
-        servos.put("arm_servo", hardwareMap.get(Servo.class, "arm_servo"));
 
         //Create and assign map entries for all CRServos
-        crservos.put("rotator_servo", hardwareMap.get(CRServo.class, "rotator_servo"));
 
         //Reset encoders
         for (String key : motors.keySet()) {
@@ -89,18 +137,17 @@ public class MainTeleOp extends LinearOpMode {
         motors.get("front_right").setDirection(DcMotor.Direction.FORWARD);
         motors.get("back_right").setDirection(DcMotor.Direction.FORWARD);
 
-        motors.get("slide").setDirection(DcMotor.Direction.REVERSE);
-        motors.get("arm").setDirection(DcMotor.Direction.FORWARD);
-        motors.get("actuator").setDirection(DcMotor.Direction.FORWARD);
+        //Initialize custom gamepads
+        custom_gamepad_1 = new CustomGamepad(gamepad1);
+        custom_gamepad_2 = new CustomGamepad(gamepad2);
 
         //Turn on the brakes for 0 power
         for (String key : motors.keySet()) {
-            motors.get(key).setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);;
+            motors.get(key).setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            ;
         }
 
         //Set direction of servos
-        servos.get("slide_servo").setDirection(Servo.Direction.FORWARD);
-        servos.get("arm_servo").setDirection(Servo.Direction.REVERSE);
 
         //This data is displayed on the driver hub console
         telemetry.addData("Status", "Initialized");
@@ -113,25 +160,78 @@ public class MainTeleOp extends LinearOpMode {
         runtime.reset();
 
         //Motor powers
-        wheel_motor_powers.put("front_left", 0.0);
-        wheel_motor_powers.put("front_right", 0.0);
-        wheel_motor_powers.put("back_left", 0.0);
-        wheel_motor_powers.put("back_right", 0.0);
-
-        other_motor_powers.put("slide", 0.0);
-        other_motor_powers.put("arm", 0.0);
-        other_motor_powers.put("actuator", 0.0);
+        motor_powers.put("front_left", 0.0);
+        motor_powers.put("front_right", 0.0);
+        motor_powers.put("back_left", 0.0);
+        motor_powers.put("back_right", 0.0);
 
         //Settings for servos
-        servo_positions.put("slide_servo", 0.0);
-        servo_positions.put("arm_servo", 0.0);
 
         //CRServos Powers
-        crservo_powers.put("rotator_servo", 0.0);
 
         //Main loop. This runs until stop is pressed on the driver hub
         while (opModeIsActive()) {
+            //Update gamepad input
+            custom_gamepad_1.update();
+            custom_gamepad_2.update();
 
+            //Manual movement controls
+            //We use gamepad1 for actually moving the robot and gamepad2 for everything else
+
+            //Controls latitudinal and longitudinal movement with left stick
+            //Vertical movement (up is negative on the joystick)
+            axial = custom_gamepad_1.get_left_stick_x(LONG_LAT_ANGULAR_DEADZONE, LONG_LAT_RADIAL_DEADZONE);
+            //Horizontal movement
+            lateral = custom_gamepad_1.get_left_stick_x(LONG_LAT_ANGULAR_DEADZONE, LONG_LAT_RADIAL_DEADZONE);
+            //Rotation calculation
+            trigger_yaw = ROTATION_SPEED * (-custom_gamepad_1.get_left_trigger() + custom_gamepad_1.get_right_trigger());
+        }
+        stick_yaw = ROTATION_SPEED * (custom_gamepad_1.get_right_stick_x(ROTATION_ANGULAR_DEADZONE, ROTATION_RADIAL_DEADZONE));
+
+        //Update manual movement state
+        if ((Math.abs(axial) + Math.abs(lateral) + Math.abs(trigger_yaw) + Math.abs(stick_yaw)) > 0) {
+            if (!check_mask("manual_movement")) {
+                //Set the manual movement byte to on
+                action_map.put("manual_movement", (byte) (action_map.get("manual_movement") | ON_BITMASK));
+            }
+
+            if (Math.abs(trigger_yaw) > 0) {
+                if (!check_mask("trigger_rotation")) {
+                    //Set the trigger movement byte to on
+                    action_map.put("trigger_rotation", (byte) (action_map.get("trigger_rotation") | ON_BITMASK));
+                }
+            } else if (Math.abs(stick_yaw) > 0) {
+                if (!check_mask("stick_rotation")) {
+                    //Set the trigger movement byte to on
+                    action_map.put("stick_rotation", (byte) (action_map.get("stick_rotation") | ON_BITMASK));
+                }
+            }
+        } else {
+            //Set the manual movement byte to off
+            action_map.put("manual_movement", (byte) (action_map.get("manual_movement") & (~ON_BITMASK)));
+        }
+
+        //Execute state actions
+        if (((byte) action_map.get("manual_movement") & ON_BITMASK) > 0) {
+            //Check which yaw we're using (if any)
+            if (!check_mask("trigger_rotation")) {
+                yaw = trigger_yaw;
+            } else if (!check_mask("stick_rotation")) {
+                yaw = stick_yaw;
+            } else {
+                yaw = 0;
+            }
+
+            //Set engine powers
+            motor_powers.put("front_left", axial + lateral + yaw);
+            motor_powers.put("front_right", axial - lateral - yaw);
+            motor_powers.put("back_left", axial - lateral + yaw);
+            motor_powers.put("back_right", axial + lateral - yaw);
+        }
+
+        //Set engine powers
+        for (String key : motor_powers.keySet()) {
+            motors.get(key).setPower(motor_powers.get(key));
         }
     }
 }
